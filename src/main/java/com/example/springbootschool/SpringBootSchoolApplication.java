62ea918657c5c2210987f9c9617b4fc1e6ad6c9f
Joonas Harjukallio <harjukallio@gmail.com>
2019-04-24 17:52:41 +0300

    Initial commit

---
+++ src/main/java/com/example/springbootschool/SpringBootSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSchoolApplication.class, args);
+	}
+
+}
